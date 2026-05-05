7b83e1c5fd2b40a23f0f087d59d436e4ef067748
Lewis Barclay <lewis.barclay@gmail.com>
2026-05-06 00:29:35 +0100

    acig v1.3.0

--- Casks/acig.rb
+++ Casks/acig.rb
@@ -1,6 +1,6 @@
 cask "acig" do
-  version "1.2.1"
-  sha256 "11b2859852cdcb547385a30fe743c964b61a2dca62e17f495f309601fa5adfa4"
+  version "1.3.0"
+  sha256 "9186a84bd458ddeac6bd001024a63fd53b37c9a0dcfd603ea9549da1d567e8d9"
 
   url "https://github.com/helloodokai/acig/releases/download/v#{version}/acig_darwin_arm64.tar.gz"
   name "acig"
@@ -10,7 +10,7 @@ cask "acig" do
   binary "acig"
 
   on_intel do
-    sha256 "a843156bce3bf85d82e56bea6ac6362aee5fe36024edc3ce3b841829d3b79bef"
+    sha256 "91278e379b7e54c33d8f3515484846cbd8fbecb10c740894b37084d3c920adaf"
     url "https://github.com/helloodokai/acig/releases/download/v#{version}/acig_darwin_amd64.tar.gz"
   end
 end
